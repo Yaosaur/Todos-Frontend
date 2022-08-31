ed063b32eb4c6dfd09db8c05ed5029b6e93679f9
Yao Jiang <yaojiang7595@gmail.com>
2022-08-31 15:01:49 -0400

    changed heroku endpoints

--- src/services/todos-api.js
+++ src/services/todos-api.js
@@ -2,29 +2,29 @@
 import axios from 'axios';
 
 export async function getTodos() {
-  const URL = 'https://todostodostodos.herokuapp.com/';
+  const URL = 'https://todostodostodos.herokuapp.com/todos';
   const response = await axios.get(URL);
   return response;
 }
 
 export async function getTodo(id) {
-  const URL = `https://todostodostodos.herokuapp.com/${id}`;
+  const URL = `https://todostodostodos.herokuapp.com/todos/${id}`;
   const response = await axios.get(URL);
   return response;
 }
 
 export async function deleteTodo(id) {
-  const URL = `https://todostodostodos.herokuapp.com/${id}`;
+  const URL = `https://todostodostodos.herokuapp.com/todos/${id}`;
   return await axios.delete(URL);
 }
 
 export async function editTodo(id, updatedTodo) {
-  const URL = `https://todostodostodos.herokuapp.com/${id}`;
+  const URL = `https://todostodostodos.herokuapp.com/todos/${id}`;
   return await axios.put(URL, updatedTodo);
 }
 
 export async function createTodo(todo) {
-  const URL = `https://todostodostodos.herokuapp.com/`;
+  const URL = `https://todostodostodos.herokuapp.com/todos`;
   const response = await axios.post(URL, todo);
   return response;
 }
